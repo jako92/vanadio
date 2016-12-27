9b235df3c4bdfe66d800505ef1da17d99a6fac32
MarioEstrada <desarrollo@desarrollo-MS-7817>
2016-12-27 16:58:04 -0500

    Servicio recursos

---
+++ src/Brasa/RecursoHumanoBundle/Admin/IncapacidadTipoAdmin.php
@@ -0,0 +1,38 @@
+<?php
+namespace Brasa\RecursoHumanoBundle\Admin;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class IncapacidadTipoAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {        
+        $formMapper->add('nombre', 'text')
+                   ->add('pagoConceptoRel', 'entity', array(
+            'class' => 'BrasaRecursoHumanoBundle:RhuPagoConcepto',
+            'choice_label' => 'nombre',
+        ))
+                ->add('generaPago', 'checkbox')
+                ->add('generaIbc', 'checkbox')
+                ->add('tipo', 'number')
+                ->add('abreviatura', 'text');        
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('nombre');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('codigoIncapacidadTipoPk')
+                ->add('nombre')
+                ->add('pagoConceptoRel.nombre')
+                ->add('generaPago')
+                ->add('generaIbc')
+                ->add('tipo')
+                ->add('abreviatura');
+    }
+}
