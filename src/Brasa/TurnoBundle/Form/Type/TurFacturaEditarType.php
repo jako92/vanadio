e610ea3993c8faa1fb603ec711d90773004170bd
MarioEstrada <desarrollo@desarrollo-MS-7817>
2016-12-29 11:29:29 -0500

    Aditar factura

--- src/Brasa/TurnoBundle/Form/Type/TurFacturaEditarType.php
+++ src/Brasa/TurnoBundle/Form/Type/TurFacturaEditarType.php
@@ -6,15 +6,27 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Doctrine\ORM\EntityRepository;
 class TurFacturaEditarType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder                             
+        $builder     
+            ->add('facturaTipoRel', EntityType::class, array(
+                'class' => 'BrasaTurnoBundle:TurFacturaTipo',
+                'query_builder' => function (EntityRepository $er)  {
+                    return $er->createQueryBuilder('ft')
+                    ->where('ft.tipo <> :tipo')
+                    ->setParameter('tipo', 2)                            
+                    ->orderBy('ft.codigoFacturaTipoPk', 'ASC');},
+                'choice_label' => 'nombre',
+                'required' => true))                                         
             ->add('numero', NumberType::class, array('required' => true))                            
             ->add('fecha', DateType::class, array('format' => 'yyyyMMdd')) 
             ->add('fechaVence', DateType::class, array('format' => 'yyyyMMdd')) 
+            ->add('descripcion', TextType::class, array('required' => false))                             
             ->add('guardar', SubmitType::class);
     }
 
