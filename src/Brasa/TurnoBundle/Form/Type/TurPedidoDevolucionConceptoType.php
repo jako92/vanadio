8f133a89d5f3bf1bd90bd466c1fba5ffc79096b5
desarrollo <desarrollo@desarrollo-MS-7817>
2017-01-12 08:24:40 -0500

    Conceptos

---
+++ src/Brasa/TurnoBundle/Form/Type/TurPedidoDevolucionConceptoType.php
@@ -0,0 +1,26 @@
+<?php
+namespace Brasa\TurnoBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class TurPedidoDevolucionConceptoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder                      
+            ->add('nombre', TextType::class, array('required' => true))             
+            ->add('guardar', SubmitType::class)
+            ->add('guardarnuevo', SubmitType::class, array('label'  => 'Guardar y Nuevo'));
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'form';
+    }
+}
+
--- src/Brasa/TurnoBundle/Repository/TurPedidoDevolucionConceptoRepository.php
+++ src/Brasa/TurnoBundle/Repository/TurPedidoDevolucionConceptoRepository.php
@@ -5,5 +5,16 @@ namespace Brasa\TurnoBundle\Repository;
 use Doctrine\ORM\EntityRepository;
 
 class TurPedidoDevolucionConceptoRepository extends EntityRepository {
-    
+    public function ListaDql($strNombre = "", $strCodigo = "") {
+        $em = $this->getEntityManager();
+        $dql   = "SELECT pdc FROM BrasaTurnoBundle:TurPedidoDevolucionConcepto pdc WHERE pdc.codigoPedidoDevolucionConceptoPk <> 0";
+        if($strNombre != "" ) {
+            $dql .= " AND pdc.nombre LIKE '%" . $strNombre . "%'";
+        }
+        if($strCodigo != "" ) {
+            $dql .= " AND pdc.codigoPedidoDevolucionConceptoPk = " . $strCodigo;
+        }        
+        $dql .= " ORDER BY pdc.nombre";
+        return $dql;
+    }                
 }
