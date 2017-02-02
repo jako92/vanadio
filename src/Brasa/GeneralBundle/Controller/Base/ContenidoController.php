8dcfce56a089778ab81f0c69a33a583049896c0e
pc <pc@pc>
2017-02-02 08:04:58 -0500

    vacacion formato

--- src/Brasa/GeneralBundle/Controller/Base/ContenidoController.php
+++ src/Brasa/GeneralBundle/Controller/Base/ContenidoController.php
@@ -22,7 +22,7 @@ class ContenidoController extends Controller
      */
     public function listaAction(Request $request) {
         $em = $this->getDoctrine()->getManager();        
-        if(!$em->getRepository('BrasaSeguridadBundle:SegPermisoDocumento')->permiso($this->getUser(), 104, 1)) {
+        if(!$em->getRepository('BrasaSeguridadBundle:SegPermisoDocumento')->permiso($this->getUser(), 138, 1)) {
             return $this->redirect($this->generateUrl('brs_seg_error_permiso_especial'));            
         }
         $paginator  = $this->get('knp_paginator');
