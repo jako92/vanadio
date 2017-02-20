c43dd6a4701ae69d0008211abd966627d5c2d4d3
sistemas <sistemas@sistemas-jefe>
2017-02-19 22:28:51 -0500

    Orden programacion

--- src/Brasa/TurnoBundle/Repository/TurProgramacionDetalleRepository.php
+++ src/Brasa/TurnoBundle/Repository/TurProgramacionDetalleRepository.php
@@ -52,7 +52,7 @@ class TurProgramacionDetalleRepository extends EntityRepository {
         if($boolEstadoAutorizado == "0") {
             $dql .= " AND p.estadoAutorizado = 0";
         }
-        $dql .= " ORDER BY p.codigoClienteFk, p.codigoPuestoFk";
+        $dql .= " ORDER BY p.codigoClienteFk, pd.codigoPuestoFk";
         return $dql;
     }
 
