2b42b921fd3cb1660eb6e93344fc3529d0305c25
pc--25 <pc--25@pc25-H81M-H>
2017-04-12 13:00:04 -0500

    Formatos

--- src/Brasa/RecursoHumanoBundle/Formatos/FormatoExamen.php
+++ src/Brasa/RecursoHumanoBundle/Formatos/FormatoExamen.php
@@ -121,7 +121,7 @@ class FormatoExamen extends \FPDF_FPDF {
 
     public function EncabezadoDetalles() {
         $this->Ln(14);
-        $header = array('COD', 'TIPO', 'TIPO EXAMEN', 'VR.EXAMEN');
+        $header = array('ID','TIPO EXAMEN', 'VR.EXAMEN');
         $this->SetFillColor(236, 236, 236);
         $this->SetTextColor(0);
         $this->SetDrawColor(0, 0, 0);
@@ -129,12 +129,12 @@ class FormatoExamen extends \FPDF_FPDF {
         $this->SetFont('', 'B', 7);
 
         //creamos la cabecera de la tabla.
-        $w = array(10, 10, 150, 20);
+        $w = array(10, 160, 20);
         for ($i = 0; $i < count($header); $i++)
             if ($i == 0 || $i == 1)
-                $this->Cell($w[$i], 4, $header[$i], 1, 0, 'L', 1);
-            else
                 $this->Cell($w[$i], 4, $header[$i], 1, 0, 'C', 1);
+            else
+                $this->Cell($w[$i], 4, $header[$i], 1, 0, 'L', 1);
 
         //Restauración de colores y fuentes
         $this->SetFillColor(224, 235, 255);
@@ -150,12 +150,11 @@ class FormatoExamen extends \FPDF_FPDF {
         $pdf->SetFont('Arial', '', 7);
         foreach ($arExamenDetalles as $arExamenDetalle) {            
             $pdf->Cell(10, 4, $arExamenDetalle->getCodigoExamenDetallePk(), 1, 0, 'L');
-            $pdf->Cell(10, 4, $arExamenDetalle->getExamenTipoRel()->getCodigoExamenTipoPk(), 1, 0, 'L');
-            $pdf->Cell(150, 4, utf8_decode($arExamenDetalle->getExamenTipoRel()->getNombre()), 1, 0, 'L');
+            $pdf->Cell(160, 4, utf8_decode($arExamenDetalle->getExamenTipoRel()->getNombre()), 1, 0, 'L');
             $pdf->Cell(20, 4, number_format($arExamenDetalle->getVrPrecio(), 0, '.', ','), 1, 0, 'R');
             $pdf->Ln();
             $pdf->SetAutoPageBreak(true, 15);
-            $w = array(10, 10, 150, 20);
+            $w = array(10, 160, 20);
         }
         $w=$pdf->getY();
         $pdf->SetXY(160,$w + 4);
