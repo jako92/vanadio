d94f2d0658874c5bc318f3ad1c62945f4014b8b4
pc--25 <pc--25@pc25-H81M-H>
2017-04-17 11:24:37 -0500

    Control de puesto

--- src/Brasa/TurnoBundle/Controller/Movimiento/ControlPuestoController.php
+++ src/Brasa/TurnoBundle/Controller/Movimiento/ControlPuestoController.php
@@ -32,7 +32,7 @@ class ControlPuestoController extends Controller {
      */
     public function listaAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
-        if (!$em->getRepository('BrasaSeguridadBundle:SegPermisoDocumento')->permiso($this->getUser(), 27, 1)) {
+        if (!$em->getRepository('BrasaSeguridadBundle:SegPermisoDocumento')->permiso($this->getUser(), 140, 1)) {
             return $this->redirect($this->generateUrl('brs_seg_error_permiso_especial'));
         }
         $paginator = $this->get('knp_paginator');
