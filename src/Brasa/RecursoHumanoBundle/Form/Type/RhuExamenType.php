ac46d83be1b4b828841a252cd4f836555f56b78f
desarrollo <desarrollo@localhost>
2017-05-03 13:12:06 -0500

    Examenes medicos

--- src/Brasa/RecursoHumanoBundle/Form/Type/RhuExamenType.php
+++ src/Brasa/RecursoHumanoBundle/Form/Type/RhuExamenType.php
@@ -53,7 +53,7 @@ class RhuExamenType extends AbstractType {
                 ->add('fechaNacimiento', DateType::class, array('widget' => 'single_text', 'format' => 'yyyy-MM-dd', 'attr' => array('class' => 'date',)))
                 ->add('controlPago', ChoiceType::class, array('choices' => array('SI' => '1', 'NO' => '0')))
                 ->add('controlPago', CheckboxType::class, array('required' => false))
-                ->add('cobro', TextType::class, array('required' => false))                            
+                ->add('cobro', ChoiceType::class, array('choices' => array('CLIENTE' => 'C', 'EMPLEADO' => 'E', 'NO COBRAR' => 'N')))                                            
                 ->add('guardar', SubmitType::class)
                 ->add('guardarnuevo', SubmitType::class, array('label' => 'Guardar y Nuevo'));
     }
