22e766fb174a06b90fd6a227724a532f44dd13c8
desarrollo <desarrollo@localhost>
2017-05-04 08:56:01 -0500

    Crear areas

---
+++ src/Brasa/InventarioBundle/Admin/InventarioAreaAdmin.php
@@ -0,0 +1,24 @@
+<?php
+namespace Brasa\InventarioBundle\Admin;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class InventarioAreaAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('nombre', 'text');
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('nombre');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('nombre');
+    }
+}
