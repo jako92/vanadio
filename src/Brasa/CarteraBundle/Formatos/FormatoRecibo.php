aeee86b0ba79282d67e14e5baade8b8d431d471c
desarrollo <desarrollo@desarrollo-MS-7817>
2017-05-24 13:40:34 -0500

    campo nota credito en relacion de cobro.

--- src/Brasa/CarteraBundle/Formatos/FormatoRecibo.php
+++ src/Brasa/CarteraBundle/Formatos/FormatoRecibo.php
@@ -160,7 +160,7 @@ class FormatoRecibo extends \FPDF_FPDF {
 
     public function EncabezadoDetalles() {
         $this->Ln(14);
-        $header = array('ID', 'TIPO', utf8_decode('NUMERO'),'DESCUENTO', 'AJUSTE PESO', 'RETE ICA', 'RETE IVA', 'RETE FUENTE', 'VALOR');
+        $header = array('ID', 'TIPO',utf8_decode('NOTA') , utf8_decode('FACTURA'),'DESCUENTO', 'AJUSTE PESO', 'RETE ICA', 'RETE IVA', 'RETE FUENTE', 'VALOR');
         $this->SetFillColor(236, 236, 236);
         $this->SetTextColor(0);
         $this->SetDrawColor(0, 0, 0);
@@ -168,7 +168,7 @@ class FormatoRecibo extends \FPDF_FPDF {
         $this->SetFont('', 'B', 7.5);
 
         //creamos la cabecera de la tabla.
-        $w = array(13, 40, 20, 20, 20, 20, 20,20,20,20);
+        $w = array(11, 33,15, 15, 20, 20, 20, 20,20,20,20);
         for ($i = 0; $i < count($header); $i++)
             if ($i == 0 || $i == 1)
                 $this->Cell($w[$i], 4, $header[$i], 1, 0, 'L', 1);
@@ -188,9 +188,10 @@ class FormatoRecibo extends \FPDF_FPDF {
         $pdf->SetX(10);
         $pdf->SetFont('Arial', '', 7);
         foreach ($arReciboDetalles as $arReciboDetalle) {            
-            $pdf->Cell(13, 4, $arReciboDetalle->getCodigoReciboDetallePk(), 1, 0, 'L');
-            $pdf->Cell(40, 4, utf8_decode($arReciboDetalle->getCuentaCobrarTipoRel()->getNombre()), 1, 0, 'L');
-            $pdf->Cell(20, 4, $arReciboDetalle->getNumeroFactura(), 1, 0, 'L');            
+            $pdf->Cell(11, 4, $arReciboDetalle->getCodigoReciboDetallePk(), 1, 0, 'L');
+            $pdf->Cell(33, 4, utf8_decode($arReciboDetalle->getCuentaCobrarTipoRel()->getNombre()), 1, 0, 'L');
+            $pdf->Cell(15, 4, $arReciboDetalle->getNumeroDocumentoAplicacion(), 1, 0, 'L');  
+            $pdf->Cell(15, 4, $arReciboDetalle->getNumeroFactura(), 1, 0, 'L');            
             $pdf->Cell(20, 4, number_format($arReciboDetalle->getVrDescuento(), 2, '.', ','), 1, 0, 'R');
             $pdf->Cell(20, 4, number_format($arReciboDetalle->getVrAjustePeso(), 2, '.', ','), 1, 0, 'R');
             $pdf->Cell(20, 4, number_format($arReciboDetalle->getVrRetencionIca(), 2, '.', ','), 1, 0, 'R');
