236d1005d5ea03d38119e28aea7bf851d5da6ef9
desarrollo <desarrollo@desarrollo-MS-7817>
2017-06-07 09:09:42 -0500

    Estandarizar fomulario de vacaciones.

--- src/Brasa/RecursoHumanoBundle/Form/Type/RhuVacacionType.php
+++ src/Brasa/RecursoHumanoBundle/Form/Type/RhuVacacionType.php
@@ -1,6 +1,7 @@
 <?php
+
 namespace Brasa\RecursoHumanoBundle\Form\Type;
- 
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
@@ -8,24 +9,22 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class RhuVacacionType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+class RhuVacacionType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('diasDisfrutados', NumberType::class, array('required' => true))                 
-            ->add('diasPagados', NumberType::class, array('required' => true))                 
-            ->add('vrSalarioPromedioPropuesto', NumberType::class, array('required' => false))                 
-            ->add('fechaDesdeDisfrute', DateType::class)
-            ->add('fechaHastaDisfrute', DateType::class) 
-            ->add('fechaInicioLabor', DateType::class) 
-            ->add('comentarios', TextareaType::class, array('required' => false))                
-            ->add('guardar', SubmitType::class);
+                ->add('diasDisfrutados', NumberType::class, array('required' => true))
+                ->add('diasPagados', NumberType::class, array('required' => true))
+                ->add('vrSalarioPromedioPropuesto', NumberType::class, array('required' => false))
+                ->add('fechaDesdeDisfrute', DateType::class, array('widget' => 'single_text', 'format' => 'yyyy-MM-dd', 'attr' => array('class' => 'date',)))
+                ->add('fechaHastaDisfrute', DateType::class, array('widget' => 'single_text', 'format' => 'yyyy-MM-dd', 'attr' => array('class' => 'date',)))
+                ->add('fechaInicioLabor', DateType::class, array('widget' => 'single_text', 'format' => 'yyyy-MM-dd', 'attr' => array('class' => 'date',)))
+                ->add('comentarios', TextareaType::class, array('required' => false))
+                ->add('guardar', SubmitType::class);
     }
- 
-    public function getBlockPrefix()
-    {
+
+    public function getBlockPrefix() {
         return 'form';
     }
-}
 
+}
