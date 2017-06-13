d8dcf6a5f05e3277c815a42c9a566c03ba7115fd
desarrollo <desarrollo@desarrollo-MS-7817>
2017-06-13 08:46:24 -0500

    puestos.

--- src/Brasa/TurnoBundle/Form/Type/TurClientePuestoType.php
+++ src/Brasa/TurnoBundle/Form/Type/TurClientePuestoType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 
 class TurClientePuestoType extends AbstractType
 {
