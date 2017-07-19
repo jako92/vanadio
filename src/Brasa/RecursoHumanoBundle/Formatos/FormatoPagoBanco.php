86cbc188d12d3354ed9c02c88c58891674ed6c8f
desarrollo <desarrollo@desarrollo-MS-7817>
2017-07-19 10:42:38 -0500

    Cambiar formato de pago banco.

--- src/Brasa/RecursoHumanoBundle/Formatos/FormatoPagoBanco.php
+++ src/Brasa/RecursoHumanoBundle/Formatos/FormatoPagoBanco.php
@@ -1,10 +1,13 @@
 <?php
+
 namespace Brasa\RecursoHumanoBundle\Formatos;
+
 class FormatoPagoBanco extends \FPDF_FPDF {
+
     public static $em;
     public static $codigoPagoBanco;
-    
-    public function Generar($em, $codigoPagoBanco) {        
+
+    public function Generar($em, $codigoPagoBanco) {
         ob_clean();
         //$em = $miThis->getDoctrine()->getManager();
         self::$em = $em;
@@ -14,98 +17,96 @@ class FormatoPagoBanco extends \FPDF_FPDF {
         $pdf->AddPage();
         $pdf->SetFont('Times', '', 12);
         $this->Body($pdf);
-        $pdf->Output("PagoBanco$codigoPagoBanco.pdf", 'D');                
-    } 
-    
+        $pdf->Output("PagoBanco$codigoPagoBanco.pdf", 'D');
+    }
+
     public function Header() {
         $arConfiguracion = new \Brasa\GeneralBundle\Entity\GenConfiguracion();
         $arConfiguracion = self::$em->getRepository('BrasaGeneralBundle:GenConfiguracion')->find(1);
         $arContenidoFormatoA = new \Brasa\GeneralBundle\Entity\GenContenidoFormatoSecundario();
         $arContenidoFormatoA = self::$em->getRepository('BrasaGeneralBundle:GenContenidoFormatoSecundario')->find(6);
-        $this->SetFillColor(200, 200, 200);        
-        $this->SetFont('Arial','B',10);
+        $this->SetFillColor(200, 200, 200);
+        $this->SetFont('Arial', 'B', 10);
         //Logo
-        
+
         $this->Image('imagenes/logos/logo.jpg', 12, 13, 35, 17);
         //INFORMACIÓN EMPRESA
         $this->SetXY(50, 10);
         $this->Cell(150, 7, utf8_decode("PAGO BANCO"), 0, 0, 'C', 1);
         $this->SetXY(50, 18);
-        $this->SetFont('Arial','B',9);
+        $this->SetFont('Arial', 'B', 9);
         $this->Cell(20, 4, "EMPRESA:", 0, 0, 'L', 1);
         $this->Cell(100, 4, $arConfiguracion->getNombreEmpresa(), 0, 0, 'L', 0);
         $this->SetXY(50, 22);
         $this->Cell(20, 4, "NIT:", 0, 0, 'L', 1);
-        $this->Cell(100, 4, $arConfiguracion->getNitEmpresa()." - ". $arConfiguracion->getDigitoVerificacionEmpresa(), 0, 0, 'L', 0);
+        $this->Cell(100, 4, $arConfiguracion->getNitEmpresa() . " - " . $arConfiguracion->getDigitoVerificacionEmpresa(), 0, 0, 'L', 0);
         $this->SetXY(50, 26);
         $this->Cell(20, 4, utf8_decode("DIRECCIÓN:"), 0, 0, 'L', 1);
         $this->Cell(100, 4, $arConfiguracion->getDireccionEmpresa(), 0, 0, 'L', 0);
         $this->SetXY(50, 30);
         $this->Cell(20, 4, utf8_decode("TELÉFONO:"), 0, 0, 'L', 1);
-        $this->Cell(100, 4, $arConfiguracion->getTelefonoEmpresa(), 0, 0, 'L', 0);        
+        $this->Cell(100, 4, $arConfiguracion->getTelefonoEmpresa(), 0, 0, 'L', 0);
         //
         $arPagoBanco = new \Brasa\RecursoHumanoBundle\Entity\RhuPagoBanco();
         $arPagoBanco = self::$em->getRepository('BrasaRecursoHumanoBundle:RhuPagoBanco')->find(self::$codigoPagoBanco);
-        $this->SetFillColor(236, 236, 236);        
-        $this->SetFont('Arial','B',10);
+        $this->SetFillColor(236, 236, 236);
+        $this->SetFont('Arial', 'B', 10);
         //linea 1
         $this->SetXY(10, 40);
-        $this->SetFillColor(200, 200, 200); 
-        $this->SetFont('Arial','B',8);
-        $this->Cell(30, 6, utf8_decode("NUMERO:") , 1, 0, 'L', 1);
-        $this->SetFillColor(272, 272, 272); 
-        $this->SetFont('Arial','',8);
-        $this->Cell(30, 6, $arPagoBanco->getNumero(), 1, 0, 'R', 1);
-        $this->SetFont('Arial','B',8);
         $this->SetFillColor(200, 200, 200);
-        $this->Cell(30, 6, "BANCO:" , 1, 0, 'L', 1);
-        $this->SetFont('Arial','',8);
-        $this->SetFillColor(272, 272, 272); 
+        $this->SetFont('Arial', 'B', 8);
+        $this->Cell(30, 6, utf8_decode("NUMERO:"), 1, 0, 'L', 1);
+        $this->SetFillColor(272, 272, 272);
+        $this->SetFont('Arial', '', 8);
+        $this->Cell(30, 6, $arPagoBanco->getNumero(), 1, 0, 'R', 1);
+        $this->SetFont('Arial', 'B', 8);
+        $this->SetFillColor(200, 200, 200);
+        $this->Cell(30, 6, "BANCO:", 1, 0, 'L', 1);
+        $this->SetFont('Arial', '', 8);
+        $this->SetFillColor(272, 272, 272);
         $this->Cell(103, 6, utf8_decode($arPagoBanco->getCuentaRel()->getBancoRel()->getNombre()), 1, 0, 'L', 1);
         //linea 2
         $this->SetXY(10, 45);
-        $this->SetFont('Arial','B',8);
+        $this->SetFont('Arial', 'B', 8);
         $this->SetFillColor(200, 200, 200);
-        $this->Cell(30, 5, utf8_decode("REGISTRO:") , 1, 0, 'L', 1);
-        $this->SetFont('Arial','',8);
+        $this->Cell(30, 5, utf8_decode("TIPO:"), 1, 0, 'L', 1);
+        $this->SetFont('Arial', '', 8);
         $this->SetFillColor(272, 272, 272);
-        $this->Cell(30, 5, $arPagoBanco->getNumeroRegistros() , 1, 0, 'R', 1);
-        $this->SetFont('Arial','B',8);
+        $this->Cell(30, 5, $arPagoBanco->getPagoBancoTipoRel()->getNombre(), 1, 0, 'L', 1);
+        $this->SetFont('Arial', 'B', 8);
         $this->SetFillColor(200, 200, 200);
-        $this->Cell(30, 5, "TIPO:" , 1, 0, 'L', 1);
-        $this->SetFont('Arial','',7);
-        $this->SetFillColor(272, 272, 272); 
-        $this->Cell(103, 5, $arPagoBanco->getPagoBancoTipoRel()->getNombre() , 1, 0, 'L', 1);
-        $this->SetFont('Arial','B',8);
-        
+        $this->Cell(30, 5, "NUMERO REGISTRO:", 1, 0, 'L', 1);
+        $this->SetFont('Arial', '', 7);
+        $this->SetFillColor(272, 272, 272);
+        $this->Cell(103, 5, $arPagoBanco->getNumeroRegistros(), 1, 0, 'R', 1);
+        $this->SetFont('Arial', 'B', 8);
+
         //linea 3
         $this->SetXY(10, 50);
-        $this->SetFont('Arial','B',8);
+        $this->SetFont('Arial', 'B', 8);
         $this->SetFillColor(200, 200, 200);
-        $this->Cell(30, 5, utf8_decode("FECHA:") , 1, 0, 'L', 1);
-        $this->SetFont('Arial','',8);
+        $this->Cell(30, 5, utf8_decode("FECHA:"), 1, 0, 'L', 1);
+        $this->SetFont('Arial', '', 8);
         $this->SetFillColor(272, 272, 272);
-        $this->Cell(30, 5, $arPagoBanco->getFecha()->format('Y/m/d') , 1, 0, 'L', 1);
-        $this->SetFont('Arial','B',8);
+        $this->Cell(30, 5, $arPagoBanco->getFecha()->format('Y/m/d'), 1, 0, 'L', 1);
+        $this->SetFont('Arial', 'B', 8);
         $this->SetFillColor(200, 200, 200);
-        $this->Cell(30, 5, "TOTAL:" , 1, 0, 'R', 1);
-        $this->SetFont('Arial','',8);
-        $this->SetFillColor(272, 272, 272); 
-        $this->Cell(103, 5, number_format($arPagoBanco->getVrTotalPago(), 0, '.', ',') , 1, 0, 'R', 1);
-        $this->SetFont('Arial','B',8);
-        $this->EncabezadoDetalles();
-        
+        $this->Cell(30, 5, "TOTAL:", 1, 0, 'R', 1);
+        $this->SetFont('Arial', '', 8);
+        $this->SetFillColor(272, 272, 272);
+        $this->Cell(103, 5, number_format($arPagoBanco->getVrTotalPago(), 0, '.', ','), 1, 0, 'R', 1);
+        $this->SetFont('Arial', 'B', 8);
+
         //linea 4
         $this->SetXY(10, 55);
-        $this->SetFont('Arial','B',8);
+        $this->SetFont('Arial', 'B', 8);
         $this->SetFillColor(200, 200, 200);
-        $this->Cell(30, 5, utf8_decode("DESCRIPCION:") , 1, 0, 'L', 1);
-        $this->SetFont('Arial','',8);
+        $this->Cell(30, 5, utf8_decode("DESCRIPCION:"), 1, 0, 'L', 1);
+        $this->SetFont('Arial', '', 8);
         $this->SetFillColor(272, 272, 272);
-        $this->Cell(163, 5, $arPagoBanco->getDescripcion() , 1, 0, 'L', 1);
-        $this->SetFont('Arial','B',8);
+        $this->Cell(163, 5, $arPagoBanco->getDescripcion(), 1, 0, 'L', 1);
+        $this->SetFont('Arial', 'B', 8);
         $this->EncabezadoDetalles();
-        
     }
 
     public function EncabezadoDetalles() {
@@ -135,43 +136,42 @@ class FormatoPagoBanco extends \FPDF_FPDF {
     public function Body($pdf) {
         $arPagoBancoDetalles = new \Brasa\RecursoHumanoBundle\Entity\RhuPagoBancoDetalle();
         $arPagoBancoDetalles = self::$em->getRepository('BrasaRecursoHumanoBundle:RhuPagoBancoDetalle')->findBy(array('codigoPagoBancoFk' => self::$codigoPagoBanco));
-        
+
         $pdf->SetX(10);
         $pdf->SetFont('Arial', '', 7);
         $var = 0;
-        foreach ($arPagoBancoDetalles as $arPagoBancoDetalle) {            
+        foreach ($arPagoBancoDetalles as $arPagoBancoDetalle) {
             $pdf->Cell(12, 4, $arPagoBancoDetalle->getCodigoPagoBancoDetallePk(), 1, 0, 'L');
-            if($arPagoBancoDetalle->getCodigoPagoFk()) {
-                $pdf->Cell(16, 4, $arPagoBancoDetalle->getPagoRel()->getNumero(), 1, 0, 'L');    
-            } elseif($arPagoBancoDetalle->getCodigoLiquidacionFk()) {
-                $pdf->Cell(16, 4,$arPagoBancoDetalle->getCodigoLiquidacionFk(), 1, 0, 'L');    
+            if ($arPagoBancoDetalle->getCodigoPagoFk()) {
+                $pdf->Cell(16, 4, $arPagoBancoDetalle->getPagoRel()->getNumero(), 1, 0, 'L');
+            } elseif ($arPagoBancoDetalle->getCodigoLiquidacionFk()) {
+                $pdf->Cell(16, 4, $arPagoBancoDetalle->getCodigoLiquidacionFk(), 1, 0, 'L');
             } elseif ($arPagoBancoDetalle->getCodigoVacacionFk()) {
-                $pdf->Cell(16, 4,$arPagoBancoDetalle->getCodigoVacacionFk(), 1, 0, 'L');
+                $pdf->Cell(16, 4, $arPagoBancoDetalle->getCodigoVacacionFk(), 1, 0, 'L');
             } else {
-                $pdf->Cell(25, 4, "", 1, 0, 'L');    
+                $pdf->Cell(25, 4, "", 1, 0, 'L');
             }
-            
+
             $pdf->Cell(25, 4, $arPagoBancoDetalle->getEmpleadoRel()->getNumeroIdentificacion(), 1, 0, 'L');
             $pdf->Cell(75, 4, utf8_decode($arPagoBancoDetalle->getEmpleadoRel()->getNombreCorto()), 1, 0, 'L');
-            $pdf->Cell(30, 4, substr(utf8_decode($arPagoBancoDetalle->getBancoRel()->getNombre()),0,19), 1, 0, 'L');
-            $pdf->Cell(20, 4, substr(utf8_decode($arPagoBancoDetalle->getCuenta()),0,19), 1, 0, 'L');
+            $pdf->Cell(30, 4, substr(utf8_decode($arPagoBancoDetalle->getBancoRel()->getNombre()), 0, 19), 1, 0, 'L');
+            $pdf->Cell(20, 4, substr(utf8_decode($arPagoBancoDetalle->getCuenta()), 0, 19), 1, 0, 'L');
             $pdf->Cell(15, 4, number_format($arPagoBancoDetalle->getVrPago(), 0, '.', ','), 1, 0, 'R');
             $var += $arPagoBancoDetalle->getVrPago();
             $pdf->Ln();
             $pdf->SetAutoPageBreak(true, 15);
-            
         }
-            $pdf->SetFont('Arial', 'B', 7);
-            $pdf->Cell(178, 5, "TOTAL: ", 1, 0, 'R');
-            $pdf->SetFont('Arial', '', 7);
-            $pdf->Cell(15, 5, number_format($var,0, '.', ','), 1, 0, 'R');
-        
+        $pdf->SetFont('Arial', 'B', 7);
+        $pdf->Cell(178, 5, "TOTAL: ", 1, 0, 'R');
+        $pdf->SetFont('Arial', '', 7);
+        $pdf->Cell(15, 5, number_format($var, 0, '.', ','), 1, 0, 'R');
     }
 
     public function Footer() {
-        $this->SetFont('Arial','', 8);  
+        $this->SetFont('Arial', '', 8);
         $this->Text(170, 290, utf8_decode('Página ') . $this->PageNo() . ' de {nb}');
-    }    
+    }
+
 }
 
 ?>
