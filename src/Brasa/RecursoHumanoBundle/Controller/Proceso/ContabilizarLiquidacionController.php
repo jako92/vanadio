b88cf28bea96cb073978989b83e512d52a192a6d
MarioE <desarrollo@MarioE>
2017-07-19 16:40:46 -0500

    Comprobante de liquidaciones por frond end

--- src/Brasa/RecursoHumanoBundle/Controller/Proceso/ContabilizarLiquidacionController.php
+++ src/Brasa/RecursoHumanoBundle/Controller/Proceso/ContabilizarLiquidacionController.php
@@ -14,6 +14,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 class ContabilizarLiquidacionController extends Controller
 {
     var $strDqlLista = "";
+    
     /**
      * @Route("/rhu/proceso/contabilizar/liquidacion/", name="brs_rhu_proceso_contabilizar_liquidacion")
      */     
@@ -325,6 +326,51 @@ class ContabilizarLiquidacionController extends Controller
             'arComprobante' => $arComprobanteContable,            
             'form' => $form->createView()));
     }          
+    
+    /**
+     * @Route("/rhu/proceso/contabilizar/liquidacion/configurar/", name="brs_rhu_proceso_contabilizar_liquidacion_configurar")
+     */     
+    public function configurarAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();       
+        $objMensaje = new \Brasa\GeneralBundle\MisClases\Mensajes();
+        $arConfiguracion = new \Brasa\RecursoHumanoBundle\Entity\RhuConfiguracion();                    
+        $arConfiguracion = $em->getRepository('BrasaRecursoHumanoBundle:RhuConfiguracion')->find(1); 
+        $form = $this->createFormBuilder()          
+            ->add('TxtCodigoComprobante', TextType::class, array('data' => $arConfiguracion->getCodigoComprobanteLiquidacion()))    
+            ->add('BtnGuardar', SubmitType::class, array('label'  => 'Guardar',))
+            ->getForm(); 
+        $form->handleRequest($request);
+        if($form->isValid()) {            
+            if ($form->get('BtnGuardar')->isClicked()) { 
+                $arrControles = $request->request->All();
+                $codigoComprobante = $form->get('TxtCodigoComprobante')->getData();
+                $arComprobanteContable = new \Brasa\ContabilidadBundle\Entity\CtbComprobante();                    
+                $arComprobanteContable = $em->getRepository('BrasaContabilidadBundle:CtbComprobante')->find($codigoComprobante);  
+                if($arComprobanteContable) {
+                    $intCodigoCuenta = 0;
+                    foreach ($arrControles['LblCodigoCuenta'] as $intCodigoCuenta) {
+                        $arConfiguracionCuenta = new \Brasa\RecursoHumanoBundle\Entity\RhuConfiguracionCuenta();
+                        $arConfiguracionCuenta = $em->getRepository('BrasaRecursoHumanoBundle:RhuConfiguracionCuenta')->find($intCodigoCuenta);
+                        if (count($arConfiguracionCuenta) > 0) {
+                            $intConfiguracionCuenta = $arrControles['TxtCodigoCuenta' . $intCodigoCuenta];
+                            $arConfiguracionCuenta->setCodigoCuentaFk($intConfiguracionCuenta);
+                            $em->persist($arConfiguracionCuenta);
+                        }
+                        $intCodigoCuenta++;
+                    }
+                    $em->flush();                    
+                    echo "<script languaje='javascript' type='text/javascript'>window.close();window.opener.location.reload();</script>";
+                } else {
+                    $objMensaje->Mensaje("error", "El comprobante no existe");
+                }                
+            }    
+        }       
+        $arConfiguracionCuenta = $em->getRepository('BrasaRecursoHumanoBundle:RhuConfiguracionCuenta')->findBy(array('tipo' => 'LIQUIDACION'));        
+        //$arLiquidaciones = $paginator->paginate($em->createQuery($this->strDqlLista), $request->query->get('page', 1), 300);                               
+        return $this->render('BrasaRecursoHumanoBundle:Procesos/Contabilizar:liquidacionConfigurar.html.twig', array(
+            'arConfiguracionCuenta' => $arConfiguracionCuenta,
+            'form' => $form->createView()));
+    }     
     
     private function formularioLista() {
         $em = $this->getDoctrine()->getManager();
