ca2877a126615fab1bf897661e8ac585461758cb
pc--25 <pc--25@pc25-H81M-H>
2017-08-01 17:10:58 -0500

    Permiso para poligrafias

--- src/Brasa/RecursoHumanoBundle/Controller/Movimiento/PoligrafiaController.php
+++ src/Brasa/RecursoHumanoBundle/Controller/Movimiento/PoligrafiaController.php
@@ -20,7 +20,7 @@ class PoligrafiaController extends Controller {
      */
     public function listaAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
-        if (!$em->getRepository('BrasaSeguridadBundle:SegPermisoDocumento')->permiso($this->getUser(), 119, 1)) {
+        if (!$em->getRepository('BrasaSeguridadBundle:SegPermisoDocumento')->permiso($this->getUser(), 141, 1)) {
             return $this->redirect($this->generateUrl('brs_seg_error_permiso_especial'));
         }
         $paginator = $this->get('knp_paginator');
